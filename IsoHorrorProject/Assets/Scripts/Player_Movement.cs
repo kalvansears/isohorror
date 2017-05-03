45d8d3d7cce048e0cd3f28245b0d68fe8a8a855f
K Sears <kalvansears@gmail.com>
2017-05-02 21:33:13 -0600

    Opening Project

---
+++ IsoHorrorProject/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class Player_Movement : MonoBehaviour {
+
+    KeyCode keyUp = KeyCode.A;
+    KeyCode keyLeft = KeyCode.S;
+    KeyCode keyDown = KeyCode.D;
+    KeyCode keyRight = KeyCode.W;
+
+   
+
+    public float speed = .5f;
+
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+    
+
+        if (Input.GetKey(keyUp))
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(keyDown))
+        {
+            transform.Translate(-transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(keyLeft))
+        {
+            transform.Translate(-transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(keyRight))
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+
+		
+	}
+}
---
+++ IsoHorrorProject/Assets/Scripts/Wall_Rotate.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_Rotate: MonoBehaviour {
+
+    KeyCode Clockwise = KeyCode.RightArrow;
+    KeyCode CounterClock = KeyCode.LeftArrow;
+
+
+	public GameObject Deg0_90;
+	public GameObject Deg0_Neg90;
+	public GameObject Deg180_90;
+	public GameObject Deg180_Neg90;
+
+
+
+    private Renderer Deg0_90_rend;
+    private Renderer Deg0_Neg90_rend;
+    private Renderer Deg180_90_rend;
+    private Renderer Deg180_Neg90_rend;
+
+
+    
+	// Update is called once per frame
+	void Update () {
+
+        // Key Commands for Rotation
+        
+		if (Input.GetKeyDown(Clockwise))
+			transform.Rotate (Vector3.up, 90);
+		if (Input.GetKeyDown (CounterClock))
+			transform.Rotate (Vector3.up, -90);
+
+        // Hiding Walls
+
+
+        Deg0_90_rend = Deg0_90.GetComponent<Renderer>();
+        Deg0_Neg90_rend = Deg0_Neg90.GetComponent<Renderer>();
+        Deg180_90_rend = Deg180_90.GetComponent<Renderer>();
+        Deg180_Neg90_rend = Deg180_Neg90.GetComponent<Renderer>();
+
+        Renderer[] renderChild090 = Deg0_90.GetComponentsInChildren<Renderer>();
+        Renderer[] renderChild0N90 = Deg0_Neg90.GetComponentsInChildren<Renderer>();
+        Renderer[] renderChild18090 = Deg180_90.GetComponentsInChildren<Renderer>();
+        Renderer[] renderChild180N90 = Deg180_Neg90.GetComponentsInChildren<Renderer>();
+
+        
+
+        if (transform.eulerAngles.y == 0)
+        {
+            Deg0_90_rend.enabled = true;
+            ChildRender(renderChild090, true);
+
+            Deg0_Neg90_rend.enabled = true;
+            ChildRender(renderChild0N90, true);
+
+            Deg180_90_rend.enabled = false;
+            ChildRender(renderChild18090, false);
+
+            Deg180_Neg90_rend.enabled = false;
+            ChildRender(renderChild180N90, false);
+
+
+        }
+
+        else if (transform.eulerAngles.y == 90)
+        {
+            Deg0_90_rend.enabled = true;
+            ChildRender(renderChild090, true);
+
+            Deg0_Neg90_rend.enabled = false;
+            ChildRender(renderChild0N90, false);
+
+            Deg180_90_rend.enabled = true;
+            ChildRender(renderChild18090, true);
+
+            Deg180_Neg90_rend.enabled = false;
+            ChildRender(renderChild180N90, false);
+        }
+
+        else if (transform.eulerAngles.y == 180)
+        {
+            Deg0_90_rend.enabled = false;
+            ChildRender(renderChild090, false);
+
+            Deg0_Neg90_rend.enabled = false;
+            ChildRender(renderChild0N90, false);
+
+            Deg180_90_rend.enabled = true;
+            ChildRender(renderChild18090, true);
+
+            Deg180_Neg90_rend.enabled = true;
+            ChildRender(renderChild180N90, true);
+
+        }
+
+        else
+        {
+            Deg0_90_rend.enabled = false;
+            ChildRender(renderChild090, false);
+
+            Deg0_Neg90_rend.enabled = true;
+            ChildRender(renderChild0N90, true);
+
+            Deg180_90_rend.enabled = false;
+            ChildRender(renderChild18090, false);
+
+            Deg180_Neg90_rend.enabled = true;
+            ChildRender(renderChild180N90, true);
+
+        }
+	}
+
+    public void ChildRender(Renderer[] renderChild, bool yesno)
+    {
+        foreach (Renderer r in renderChild)
+            r.enabled = yesno;
+
+    }
+
+
+
+}
+
+
